36dcb1ec397c6816f192314c87a9318be961e00c
ricardoac4 <86529047+ricardoac4@users.noreply.github.com>
2025-06-10 23:30:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showActivities() {
+  document.getElementById('mainImage').src = 'images/Imagen2.png';
+}
+
+function showReminders() {
+  document.getElementById('mainImage').src = 'images/Imagen3.png';
+}
